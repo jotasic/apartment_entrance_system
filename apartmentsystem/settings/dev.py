9e271cd6627bdb769c341add41252b35e9629309
TaewooKim <embedded61@gmail.com>
2021-07-14 17:22:38 +0900

    Modify : 도커 환경 설정 수정

---
+++ apartmentsystem/settings/dev.py
@@ -0,0 +1,18 @@
+from .base   import *
+
+SECRET_KEY = get_env_variable('DJANGO_SECRECT_KEY')
+DEBUG      = get_env_variable('DJANGO_APP_DEBUG')
+
+DATABASES = {
+    'default' : {
+        'ENGINE': get_env_variable('SQL_ENGINE'),
+        'NAME': get_env_variable('SQL_DATABASE'),
+        'USER': get_env_variable('SQL_USER'),
+        'PASSWORD': get_env_variable('SQL_PASSWORD'),
+        'HOST':  get_env_variable('SQL_HOST'),
+        'PORT': get_env_variable('SQL_PORT'),
+        'OPTIONS' : {
+            'charset' : 'utf8mb4'
+        }
+    }
+}
