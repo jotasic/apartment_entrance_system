1dac3b866147ccb6ced0df3e135758c614057e9c
TaewooKim <embedded61@gmail.com>
2021-07-16 17:01:13 +0900

    Modify: door열때 실패시 400대신 401을 반환하도록 수정

--- api/doors/tests.py
+++ api/doors/tests.py
@@ -29,4 +29,4 @@ class AdminDoorTestCase(APITestCase):
                                     data=json.dumps(data), 
                                     content_type='application/json')
 
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- api/doors/views.py
+++ api/doors/views.py
@@ -1,6 +1,9 @@
+from django.http.response import Http404
 from rest_framework          import status, serializers
 from rest_framework.views    import APIView
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404 
+
 
 from .serializers            import DoorUseLogSerializer, DoorSearchSerializer
 from .models                 import DoorUseLog
@@ -11,14 +14,14 @@ class DoorSearchAPIView(APIView):
             serializer = DoorSearchSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
 
-            door_use_log = DoorUseLog.objects.get(door_number=serializer.data['door_number'], 
+            obj = get_object_or_404(DoorUseLog, door_number=serializer.data['door_number'], 
                                                     password=serializer.data['password'])
-            serializer = DoorUseLogSerializer(instance=door_use_log, many=False)
-            
+
+            serializer = DoorUseLogSerializer(instance=obj, many=False)
             return Response(serializer.data, status=status.HTTP_200_OK)
             
-        except DoorUseLog.DoesNotExist:
-            return Response({'message':'INVALID_INPUT'}, status=status.HTTP_400_BAD_REQUEST)
+        except Http404:
+            return Response({'message':'INVALID_INPUT'}, status=status.HTTP_401_UNAUTHORIZED)
 
         except serializers.ValidationError:
-            return Response({'message':'INVALID_INPUT'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message':'INVALID_INPUT'}, status=status.HTTP_401_UNAUTHORIZED)
